b81d3a83620afd3b32a928611c58f0a4a660c9c3
Sand Man <rajeshbhadra62@gmail.com>
2023-07-19 19:48:49 +0530

    sign up page design completed

---
+++ app/src/main/java/com/sharebysocial/authapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.sharebysocial.authapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Hide status bar
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //signUp button
+    }
+}
