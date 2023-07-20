e0c26c1c695ed78b9be2ea5d022902871d9dc8ee
Sand Man <rajeshbhadra62@gmail.com>
2023-07-20 16:01:27 +0530

    signup page backend is developing

--- app/src/main/java/com/sharebysocial/authapplication/MainActivity.java
+++ app/src/main/java/com/sharebysocial/authapplication/MainActivity.java
@@ -1,22 +1,97 @@
 package com.sharebysocial.authapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    String TAG;
+    CardView signUpBtn;
+    EditText email, password, rePassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TAG = "mainActivitiesTag";
         //Hide status bar
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //signUp button
+        mAuth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.signUpEmailId);
+        password = findViewById(R.id.signUpPasswordId);
+        rePassword = findViewById(R.id.renterPasswordId);
+        signUpBtn = findViewById(R.id.signUpRegisterBtnId);
+        registerBtn(); // When click on register btn
+
+    }
+
+    private void registerBtn() {
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s_email = email.getText().toString();
+                String s_password = password.getText().toString();
+                String s_rePassword = rePassword.getText().toString();
+                if (!s_email.equals("")) {
+                    if (s_rePassword.equals(s_password) && !s_password.equals("")) {
+                        createAccount(s_email, s_password);
+                    } else {
+                        Toast.makeText(MainActivity.this, "Password is not matched or Password is empty", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Please enter email", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+    public void createAccount(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+//                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
     }
 }
